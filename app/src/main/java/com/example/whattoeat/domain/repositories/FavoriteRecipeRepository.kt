7b67db2cc83bb36ecc06022fbc8b942b6b56e2fa
Arthas <rvr280206@gmail.com>
2026-02-03 08:43:30 +0300

    feat: created get methods for users and favorits RecipeRepos

--- app/src/main/java/com/example/whattoeat/domain/repositories/FavoriteRecipeRepository.kt
+++ app/src/main/java/com/example/whattoeat/domain/repositories/FavoriteRecipeRepository.kt
@@ -1,10 +1,14 @@
 package com.example.whattoeat.domain.repositories
 
 import com.example.whattoeat.domain.domain_entities.common.Recipe
+import com.example.whattoeat.domain.search.RecipeSearch
+import kotlinx.coroutines.flow.Flow
 
 interface FavoriteRecipeRepository {
 
     suspend fun addRecipe(recipe: Recipe): Long
 
     suspend fun removeRecipe(recipe: Recipe): Int
+
+    suspend fun getRecipes(recipeSearch: RecipeSearch): Flow<Recipe>
 }
