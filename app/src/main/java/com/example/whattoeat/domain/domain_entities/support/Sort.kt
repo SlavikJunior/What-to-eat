81d0e1df0e98891752a00a3bd294c84071e16e3c
Arthas <rvr280206@gmail.com>
2026-02-03 14:15:57 +0300

    fix: prepared for serialization

--- app/src/main/java/com/example/whattoeat/domain/domain_entities/support/Sort.kt
+++ app/src/main/java/com/example/whattoeat/domain/domain_entities/support/Sort.kt
@@ -1,23 +1,23 @@
 package com.example.whattoeat.domain.domain_entities.support
 
-enum class SortTypes {
+enum class SortTypes(val value: String) {
 
-    POPULARITY,
-    HEALTHINESS,
-    PRICE,
-    TIME,
-    MAX_USED_INGREDIENTS,
-    MIN_MISSING_INGREDIENTS,
-    ENERGY,
-    CALORIES,
-    CARBS,
-    TOTAL_FAT,
-    PROTEIN,
-    SUGAR
+    POPULARITY(value = "popularity"),
+    HEALTHINESS(value = "healthiness"),
+    PRICE(value = "price"),
+    TIME(value = "time"),
+    MAX_USED_INGREDIENTS(value = "max-used-ingredients"),
+    MIN_MISSING_INGREDIENTS(value = "min-missing-ingredients"),
+    ENERGY(value = "energy"),
+    CALORIES(value = "calories"),
+    CARBS(value = "carbs"),
+    TOTAL_FAT(value = "total-fat"),
+    PROTEIN(value = "protein"),
+    SUGAR(value = "sugar")
 }
 
-enum class SortDirection {
+enum class SortDirection(val value: String) {
 
-    ASC,
-    DESC
+    ASC(value = "asc"),
+    DESC(value = "desc")
 }
