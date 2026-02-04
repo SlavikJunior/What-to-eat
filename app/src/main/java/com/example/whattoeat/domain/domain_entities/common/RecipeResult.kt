c9a99a63383ac0e3d4d8d4cca61376253492691e
Arthas <rvr280206@gmail.com>
2026-02-05 00:33:56 +0300

    fix: Sealed Interfaces implemetations moved into intarfase's files

--- app/src/main/java/com/example/whattoeat/domain/domain_entities/common/RecipeResult.kt
+++ app/src/main/java/com/example/whattoeat/domain/domain_entities/common/RecipeResult.kt
@@ -1,3 +1,30 @@
 package com.example.whattoeat.domain.domain_entities.common
 
-sealed interface RecipeResult
+import kotlinx.serialization.Serializable
+
+sealed interface RecipeResult {
+
+    @JvmInline
+    @Serializable
+    value class RecipeByIngredientsResult(val recipeByIngredientsResult: List<Recipe.RecipeByIngredients>): RecipeResult
+
+    @Serializable
+    data class RecipeComplexResult(
+        val recipeComplexList: List<Recipe.RecipeComplex>,
+        val offset: Int,
+        val number: Int,
+        val totalResults: Int
+    ): RecipeResult
+
+    @JvmInline
+    @Serializable
+    value class RecipeFullInformationResult(val recipeFullInformationResult: Recipe.RecipeFullInformation): RecipeResult
+
+    @JvmInline
+    @Serializable
+    value class RecipeSimilarResult(val recipeSimilarResult: List<Recipe.RecipeSimilar>): RecipeResult
+
+    @JvmInline
+    @Serializable
+    value class RecipeSummaryResult(val recipeSummary: Recipe.RecipeSummary): RecipeResult
+}
